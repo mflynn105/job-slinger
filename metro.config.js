c8de962801d1aa75d1f132695b5a3cbcd1e606a0
mflynn105 <mflynn105@gmail.com>
2025-08-07 20:33:18 -0700

    Create metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,4 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.sourceExts.push('cjs', 'mjs');
+module.exports = defaultConfig;
